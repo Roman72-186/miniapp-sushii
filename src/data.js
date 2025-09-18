c1cb3874d652e621621d3a375c50dde395c6434b
Roman Mahmetov <mahmetov1988@bk.ru>
2025-09-18 22:58:01 +0500

    фото

--- src/data.js
+++ src/data.js
@@ -2,211 +2,211 @@ export const products = [
   {
     id: 1,
     name: "Авокадо маки",
-    price: 250,
+    price: 0,
     image: "/img/avokado_maki.PNG"
   },
   {
     id: 2,
     name: "Аге гурме",
-    price: 320,
+    price: 0,
     image: "/img/age_gurme.PNG"
   },
   {
     id: 3,
     name: "Аляска кунсей",
-    price: 340,
+    price: 0,
     image: "/img/alaska_kunsei.PNG"
   },
   {
     id: 4,
     name: "Благородный сёгун",
-    price: 390,
+    price: 0,
     image: "/img/seegun.PNG"
   },
   {
     id: 5,
     name: "Бонито мидии",
-    price: 300,
+    price: 0,
     image: "/img/bonito_midii.PNG"
   },
   {
     id: 6,
     name: "Бонито тунец",
-    price: 310,
+    price: 0,
     image: "/img/bonito_tunec.PNG"
   },
   {
     id: 7,
     name: "Боул с креветками",
-    price: 420,
+    price: 0,
     image: "/img/boul_s_krevetkami.PNG"
   },
   {
     id: 8,
     name: "Боул с лососем",
-    price: 450,
+    price: 0,
     image: "/img/boul_s_lososem.PNG"
   },
   {
     id: 9,
     name: "Боул с тунцом",
-    price: 430,
+    price: 0,
     image: "/img/boul_s_tuncom.PNG"
   },
   {
     id: 10,
     name: "Бруклин",
-    price: 400,
+    price: 0,
     image: "/img/bruklin.PNG"
   },
   {
     id: 11,
     name: "Гламур",
-    price: 370,
+    price: 0,
     image: "/img/glamur.PNG"
   },
   {
     id: 12,
     name: "Горячий дон жуан",
-    price: 390,
+    price: 0,
     image: "/img/don_juan.PNG"
   },
   {
     id: 13,
     name: "Гучии",
-    price: 350,
+    price: 0,
     image: "/img/gujji.PNG"
   },
   {
     id: 14,
     name: "Детройт",
-    price: 410,
+    price: 0,
     image: "/img/detroit.PNG"
   },
   {
     id: 15,
     name: "Запеченный лайт",
-    price: 200,
+    price: 0,
     image: "/img/zapecheni_lite.PNG"
   },
   {
     id: 16,
     name: "Запеченный лосось файр",
-    price: 370,
+    price: 0,
     image: "/img/losos_fair.PNG"
   },
   {
     id: 17,
     name: "Запеченный Мидуэй",
-    price: 380,
+    price: 0,
     image: "/img/miduei(1).PNG"
   },
   {
     id: 18,
     name: "Запеченный мистер Грин",
-    price: 360,
+    price: 0,
     image: "/img/Miduei.PNG"
   },
   {
     id: 19,
     name: "Запеченный ролл с беконом",
-    price: 400,
+    price: 0,
     image: "/img/roll_s_bekonom.PNG"
   },
   {
     id: 20,
     name: "Запеченный тори ролл",
-    price: 390,
+    price: 0,
     image: "/img/tori_roll.PNG"
   },
   {
     id: 21,
     name: "Игуана",
-    price: 350,
+    price: 0,
     image: "/img/iguana.PNG"
   },
   {
     id: 22,
     name: "Калифорния",
-    price: 330,
+    price: 0,
     image: "/img/kalifornia.PNG"
   },
   {
     id: 23,
     name: "Кани гриль",
-    price: 370,
+    price: 0,
     image: "/img/kani_gril.PNG"
   },
   {
     id: 24,
     name: "Каппа маки",
-    price: 250,
+    price: 0,
     image: "/img/kappa_maki.PNG"
   },
   {
     id: 25,
     name: "Киото чикен",
-    price: 390,
+    price: 0,
     image: "/img/kioto_chiken.PNG"
   },
   {
     id: 26,
     name: "Краб дуэт гриль",
-    price: 420,
+    price: 0,
     image: "/img/krab_duet.PNG"
   },
   {
     id: 27,
     name: "Маленький принц",
-    price: 310,
+    price: 0,
     image: "/img/mal_princ.PNG"
   },
   {
     id: 28,
     name: "Манхеттен",
-    price: 400,
+    price: 0,
     image: "/img/manheten.PNG"
   },
   {
     id: 29,
     name: "Мидии маки гриль",
-    price: 350,
+    price: 0,
     image: "/img/midii_maki_gril.PNG"
   },
   {
     id: 30,
     name: "Мидии терияки",
-    price: 360,
+    price: 0,
     image: "/img/midii_teriaki.PNG"
   },
   {
     id: 31,
     name: "Нежный поцелуй",
-    price: 380,
+    price: 0,
     image: "/img/nejni_kiss.PNG"
   },
   {
     id: 32,
     name: "Ниагара",
-    price: 400,
+    price: 0,
     image: "/img/niagara.PNG"
   },
   {
     id: 33,
     name: "Пикантный лосось",
-    price: 420,
+    price: 0,
     image: "/img/picantnii_losos.jpg"
   },
   {
     id: 34,
     name: "Розовый",
-    price: 350,
+    price: 0,
     image: "/img/pink.PNG"
   },
   {
     id: 35,
     name: "Самурай",
-    price: 410,
+    price: 0,
     image: "/img/samurai.PNG"
   }
 ];
