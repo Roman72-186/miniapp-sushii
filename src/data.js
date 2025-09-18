012be85e920f04f351e71a944631e0bffe06c85d
Roman Mahmetov <mahmetov1988@bk.ru>
2025-09-18 22:53:22 +0500

    фото

--- src/data.js
+++ src/data.js
@@ -1,248 +1,212 @@
 export const products = [
-    {
-      id: 1,
-      name: "Авокадо маки",
-      // description: "Классический ролл с лососем и сливочным сыром",
-      price: 0,
-      image: "/public/img/avokado_maki.PNG"
-    },
-    {
-      id: 2,
-      name: "Аге гурме",
-      // description: "Ролл с крабом, авокадо и икрой тобико",
-      price: 0,
-      image: "/public/img/age_gurme.PNG"
-    },
-    {
-      id: 3,
-      name: "Аляска кунсей",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/alaska_kunsei.PNG"
-    },
-    {
-      id: 3,
-      name: "Благородный сёгун",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/seegun.PNG"
-    },
-    {
-      id: 3,
-      name: "Бонито мидии",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/bonito_midii.PNG"
-    },
-    {
-      id: 3,
-      name: "Бонито тунец",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/bonito_tunec.PNG"
-    },
-    {
-      id: 3,
-      name: "Боул с креветками",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/boul_s_krevetkami.PNG"
-    },
-    {
-      id: 3,
-      name: "Боул с лососем",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/boul_s_lososem.PNG"
-    },
-    {
-      id: 3,
-      name: "Боул с тунцом",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/boul_s_tuncom.PNG"
-    },
-    {
-      id: 3,
-      name: "Бруклин",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/bruklin.PNG"
-    },
-    {
-      id: 3,
-      name: "Гламур",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/glamur.PNG"
-    },
-    {
-      id: 3,
-      name: "Горячий дон жуан",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/don_juan.PNG"
-    },
-    {
-      id: 3,
-      name: "Гучии",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/gujji.PNG"
-    },
-    {
-      id: 3,
-      name: "Детройт",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/detroit.PNG"
-    },
-    {
-      id: 3,
-      name: "Запеченный лайт",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 200,
-      image: "/public/img/zapecheni_lite.PNG"
-    },
-    {
-      id: 3,
-      name: "Запеченный лосось файр",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/losos_fair.PNG"
-    },
-    {
-      id: 3,
-      name: "Запеченный Мидуэй",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/miduei(1).PNG"
-    },
-    {
-      id: 3,
-      name: "Запеченный мистер Грин",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/Miduei.PNG"
-    },
-    {
-      id: 3,
-      name: "Запеченный ролл с беконом",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/roll_s_bekonom.PNG"
-    },
-    {
-      id: 3,
-      name: "Запеченный тори ролл",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/tori_roll.PNG"
-    },
-    {
-      id: 3,
-      name: "Игуана",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/iguana.PNG"
-    },
-    {
-      id: 3,
-      name: "Калифорния",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/kalifornia.PNG"
-    },
-    {
-      id: 3,
-      name: "Кани гриль",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/kani_gril.PNG"
-    },
-    {
-      id: 3,
-      name: "Каппа маки",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/kappa_maki.PNG"
-    },
-    {
-      id: 3,
-      name: "Киото чикен",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/kioto_chiken.PNG"
-    },
-    {
-      id: 3,
-      name: "Краб дуэт гриль",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/krab_duet.PNG"
-    },
-    {
-      id: 3,
-      name: "Маленький принц",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/mal_princ.PNG"
-    },
-    {
-      id: 3,
-      name: "Манхеттен",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/manheten.PNG"
-    },
-    {
-      id: 3,
-      name: "Мидии маки гриль",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/midii_maki_gril.PNG"
-    },
-    {
-      id: 3,
-      name: "Мидии терияки",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/midii_teriaki.PNG"
-    },
-    {
-      id: 3,
-      name: "Нежный поцелуй",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/nejni_kiss.PNG"
-    },
-    {
-      id: 3,
-      name: "Ниагара",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/niagara.PNG"
-    },
-    {
-      id: 3,
-      name: "Пикантный лосось",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/picantnii_losos.jpg"
-    },
-    {
-      id: 3,
-      name: "Розовый",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/pink.PNG"
-    },
-    {
-      id: 3,
-      name: "Самурай",
-      // description: "Традиционный японский суп на основе мисо пасты",
-      price: 0,
-      image: "/public/img/samurai.PNG"
-    }
-  ];
-  
+  {
+    id: 1,
+    name: "Авокадо маки",
+    price: 250,
+    image: "/img/avokado_maki.PNG"
+  },
+  {
+    id: 2,
+    name: "Аге гурме",
+    price: 320,
+    image: "/img/age_gurme.PNG"
+  },
+  {
+    id: 3,
+    name: "Аляска кунсей",
+    price: 340,
+    image: "/img/alaska_kunsei.PNG"
+  },
+  {
+    id: 4,
+    name: "Благородный сёгун",
+    price: 390,
+    image: "/img/seegun.PNG"
+  },
+  {
+    id: 5,
+    name: "Бонито мидии",
+    price: 300,
+    image: "/img/bonito_midii.PNG"
+  },
+  {
+    id: 6,
+    name: "Бонито тунец",
+    price: 310,
+    image: "/img/bonito_tunec.PNG"
+  },
+  {
+    id: 7,
+    name: "Боул с креветками",
+    price: 420,
+    image: "/img/boul_s_krevetkami.PNG"
+  },
+  {
+    id: 8,
+    name: "Боул с лососем",
+    price: 450,
+    image: "/img/boul_s_lososem.PNG"
+  },
+  {
+    id: 9,
+    name: "Боул с тунцом",
+    price: 430,
+    image: "/img/boul_s_tuncom.PNG"
+  },
+  {
+    id: 10,
+    name: "Бруклин",
+    price: 400,
+    image: "/img/bruklin.PNG"
+  },
+  {
+    id: 11,
+    name: "Гламур",
+    price: 370,
+    image: "/img/glamur.PNG"
+  },
+  {
+    id: 12,
+    name: "Горячий дон жуан",
+    price: 390,
+    image: "/img/don_juan.PNG"
+  },
+  {
+    id: 13,
+    name: "Гучии",
+    price: 350,
+    image: "/img/gujji.PNG"
+  },
+  {
+    id: 14,
+    name: "Детройт",
+    price: 410,
+    image: "/img/detroit.PNG"
+  },
+  {
+    id: 15,
+    name: "Запеченный лайт",
+    price: 200,
+    image: "/img/zapecheni_lite.PNG"
+  },
+  {
+    id: 16,
+    name: "Запеченный лосось файр",
+    price: 370,
+    image: "/img/losos_fair.PNG"
+  },
+  {
+    id: 17,
+    name: "Запеченный Мидуэй",
+    price: 380,
+    image: "/img/miduei(1).PNG"
+  },
+  {
+    id: 18,
+    name: "Запеченный мистер Грин",
+    price: 360,
+    image: "/img/Miduei.PNG"
+  },
+  {
+    id: 19,
+    name: "Запеченный ролл с беконом",
+    price: 400,
+    image: "/img/roll_s_bekonom.PNG"
+  },
+  {
+    id: 20,
+    name: "Запеченный тори ролл",
+    price: 390,
+    image: "/img/tori_roll.PNG"
+  },
+  {
+    id: 21,
+    name: "Игуана",
+    price: 350,
+    image: "/img/iguana.PNG"
+  },
+  {
+    id: 22,
+    name: "Калифорния",
+    price: 330,
+    image: "/img/kalifornia.PNG"
+  },
+  {
+    id: 23,
+    name: "Кани гриль",
+    price: 370,
+    image: "/img/kani_gril.PNG"
+  },
+  {
+    id: 24,
+    name: "Каппа маки",
+    price: 250,
+    image: "/img/kappa_maki.PNG"
+  },
+  {
+    id: 25,
+    name: "Киото чикен",
+    price: 390,
+    image: "/img/kioto_chiken.PNG"
+  },
+  {
+    id: 26,
+    name: "Краб дуэт гриль",
+    price: 420,
+    image: "/img/krab_duet.PNG"
+  },
+  {
+    id: 27,
+    name: "Маленький принц",
+    price: 310,
+    image: "/img/mal_princ.PNG"
+  },
+  {
+    id: 28,
+    name: "Манхеттен",
+    price: 400,
+    image: "/img/manheten.PNG"
+  },
+  {
+    id: 29,
+    name: "Мидии маки гриль",
+    price: 350,
+    image: "/img/midii_maki_gril.PNG"
+  },
+  {
+    id: 30,
+    name: "Мидии терияки",
+    price: 360,
+    image: "/img/midii_teriaki.PNG"
+  },
+  {
+    id: 31,
+    name: "Нежный поцелуй",
+    price: 380,
+    image: "/img/nejni_kiss.PNG"
+  },
+  {
+    id: 32,
+    name: "Ниагара",
+    price: 400,
+    image: "/img/niagara.PNG"
+  },
+  {
+    id: 33,
+    name: "Пикантный лосось",
+    price: 420,
+    image: "/img/picantnii_losos.jpg"
+  },
+  {
+    id: 34,
+    name: "Розовый",
+    price: 350,
+    image: "/img/pink.PNG"
+  },
+  {
+    id: 35,
+    name: "Самурай",
+    price: 410,
+    image: "/img/samurai.PNG"
+  }
+];
